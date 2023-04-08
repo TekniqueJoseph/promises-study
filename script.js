fdd0088582aac7fc020c10b81a5a80bbcf52480a
TekniqueJoseph <joejacksun@gmail.com>
2023-04-08 14:49:29 -0400

    added Promisall() method

--- script.js
+++ script.js
@@ -6,8 +6,27 @@ const promise = new Promise ((resolve, reject) => {
     }
 })
 
+const promise2 = new Promise ((resolve, reject) => {
+    setTimeout(resolve, 100, 'Peace Peace')
+})
+
+const promise3 = new Promise ((resolve, reject) => {
+    setTimeout(resolve, 1000, 'Sup Sun')
+})
+
+const promise4 = new Promise ((resolve, reject) => {
+    setTimeout(resolve, 3000, 'Got dem new Ckarks!!!')
+})
+
+Promise.all([promise, promise2, promise3, promise4])
+    .then(values => {
+        console.log(values)
+    })
+
 promise
 .then(result => result + '!')
-.then(result2 => {
-    console.log(result2)
+.then(result2 =>result2 + '?' )
+.catch(() => console.log('error'))
+.then(result3 => {
+    console.log(result3 +"!")
 })
