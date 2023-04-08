e57987abe3a53efff2a57079552de09fb7426283
TekniqueJoseph <joejacksun@gmail.com>
2023-04-08 15:09:33 -0400

    used fetch to retrieve Free fake API for testing

--- script.js
+++ script.js
@@ -1,32 +1,44 @@
-const promise = new Promise ((resolve, reject) => {
-    if (true) {
-        resolve('Brillient');
-    } else {
-        reject('Nah Sun!!')
-    }
-})
+// const promise = new Promise ((resolve, reject) => {
+//     if (true) {
+//         resolve('Brillient');
+//     } else {
+//         reject('Nah Sun!!')
+//     }
+// })
 
-const promise2 = new Promise ((resolve, reject) => {
-    setTimeout(resolve, 100, 'Peace Peace')
-})
+// const promise2 = new Promise ((resolve, reject) => {
+//     setTimeout(resolve, 100, 'Peace Peace')
+// })
 
-const promise3 = new Promise ((resolve, reject) => {
-    setTimeout(resolve, 1000, 'Sup Sun')
-})
+// const promise3 = new Promise ((resolve, reject) => {
+//     setTimeout(resolve, 1000, 'Sup Sun')
+// })
 
-const promise4 = new Promise ((resolve, reject) => {
-    setTimeout(resolve, 3000, 'Got dem new Ckarks!!!')
-})
+// const promise4 = new Promise ((resolve, reject) => {
+//     setTimeout(resolve, 3000, 'Got dem new Ckarks!!!')
+// })
 
-Promise.all([promise, promise2, promise3, promise4])
-    .then(values => {
-        console.log(values)
-    })
+// Promise.all([promise, promise2, promise3, promise4])
+//     .then(values => {
+//         console.log(values)
+//     })
 
-promise
-.then(result => result + '!')
-.then(result2 =>result2 + '?' )
-.catch(() => console.log('error'))
-.then(result3 => {
-    console.log(result3 +"!")
+// promise
+// .then(result => result + '!')
+// .then(result2 =>result2 + '?' )
+// .catch(() => console.log('error'))
+// .then(result3 => {
+//     console.log(result3 +"!")
+// })
+
+const urls = [
+    'https://jsonplaceholder.typicode.com/users',
+    'https://jsonplaceholder.typicode.com/posts',
+    'https://jsonplaceholder.typicode.com/albums'
+]
+
+Promise.all(urls.map(url => {
+    return fetch(url).then(resp=> resp.json())
+})).then(results => {
+    console.log(results)
 })
