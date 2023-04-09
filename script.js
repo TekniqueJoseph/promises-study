03c08c13e06041223ad0f230e8aa988e827eae4e
TekniqueJoseph <joejacksun@gmail.com>
2023-04-08 22:41:52 -0400

    more fetch code

--- script.js
+++ script.js
@@ -5,6 +5,20 @@
 //         reject('Nah Sun!!')
 //     }
 // })
+// promise.then(result => console.log(result))
+
+// promise
+// .then(result => result + '!')
+// .then(result2 => {
+//   console.log(result2)
+// })
+
+// promise
+// .then(result => result + '!!')
+// .then(result2 => {
+//   throw Error
+//   console.log(result2)
+// }).catch(() => console.log('error'))
 
 // const promise2 = new Promise ((resolve, reject) => {
 //     setTimeout(resolve, 100, 'Peace Peace')
@@ -45,13 +59,13 @@
 //     console.log(results[2])
 // }).catch(() => console.log('error'))
 
-const fetchStarWars = fetch('http://swapi.dev/api/people/2')
-    .then(resp => resp.json())
-    .then(console.log)
+// const fetchStarWars = fetch('http://swapi.dev/api/people/2')
+//     .then(resp => resp.json())
+//     .then(console.log)
 
-const fetchPromise = fetch("http://swapi.dev/api/people/1");
-    fetchPromise.then(resp => resp.json())
-    .then(console.log);
+// const fetchPromise = fetch("http://swapi.dev/api/people/1");
+//     fetchPromise.then(resp => resp.json())
+//     .then(console.log);
 
   
 // const fetchPromise = fetch("http://swapi.dev/api/people/1");
@@ -62,48 +76,52 @@ const fetchPromise = fetch("http://swapi.dev/api/people/1");
 // Solve the questions below:
 
 // #1) Create a promise that resolves in 4 seconds and returns "success" string
-// const quest1 = new Promise ((resolve, reject) => {
-//     setTimeout(resolve, 4000, 'Got dem new Ckarks!!!')
-// })
-
+// const promiseEx = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("success");
+//   }, 4000
+// )}).then(console.log)
 
 // #2) Run the above promise and make it console.log "success"
-const quest1 = new Promise ((resolve, reject) => {
-    setTimeout(resolve, 3000, 'Got dem new Clarks Sun!!!')
-})
-quest1.then(result => {
-    console.log(result)
-})
+// const promise2 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("success");
+//   }, 4000
+// )}).then(console.log)
+
 
 // #3) Read about Promise.resolve() and Promise.reject(). How can you make
 // the above promise shorter with Promise.resolve() and console loggin "success"
 
-const promise = Promise.resolve(
-    setTimeout(() => {
-      console.log("Suuuuuuu");
-    }, 6000)
-  );
+// const promise = Promise.resolve(
+//     setTimeout(() => {
+//       console.log("Suuuuuuu");
+//     }, 6000)
+//   );
 
 // #4) Catch this error and console log 'Ooops something went wrong'
-// Promise.reject('failed')
+const promise3 = Promise.reject(('failed'),
+    setTimeout(() => {
+      throw Error
+      console.log("Suuuuuuu");
+    }, 6000)
+  ).catch(console.log('Ooops something went wrong'));
 
-// const promised = Promise.reject('failed')
-// .catch(console.log('Ooops something went wrong'))
 
 // #5) Use Promise.all to fetch all of these people from Star Wars (SWAPI) at the same time.
 // Console.log the output and make sure it has a catch block as well.
-const urels = [
-  'http://swapi.dev/api/people/1',
-  'http://swapi.dev/api/people/2',
-  'http://swapi.dev/api/people/3',
-  'http://swapi.dev/api/people/4'
-]
+// const urels = [
+//   'http://swapi.dev/api/people/1',
+//   'http://swapi.dev/api/people/2',
+//   'http://swapi.dev/api/people/3',
+//   'http://swapi.dev/api/people/4'
+// ]
 
-Promise.all(urels.map(url => {
-    return fetch(url).then(resp=> resp.json())
-})).then(results => {
-    console.log(results)
-}).catch(() => console.log('error'))
+// Promise.all(urels.map(url => {
+//     return fetch(url).then(resp=> resp.json())
+// })).then(results => {
+//     console.log(results)
+// }).catch(() => console.log('error'))
 
 // #6) Change one of your urls above to make it incorrect and fail the promise
 // does your catch block handle i
